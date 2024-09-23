e9d5bda48c95de84a33729a948d539b2bf1c2bcb
Damian Moser <damian.moser@sensemail.ch>
2024-09-23 22:58:30 +0200

    feat: axios

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,17 +4,13 @@ const baseUrl = 'http://localhost:8080/api/'
 
 export default {
   send: async (forename: string, surname: string, email: string, message: string) => {
-    axios.post(
-      `${baseUrl}email`,
-      {
-        forename: forename,
-        surname: surname,
-        email: email,
-        message: message
-      },
-      {
-        withCredentials: true
-      }
-    )
+    const res = await axios.post(`${baseUrl}email`, {
+      forename: forename,
+      surname: surname,
+      email: email,
+      message: message
+    })
+
+    return res
   }
 }
