a548cd7146a3dba495a2ef1348f49d05e195de8b
Damian Moser <damian.moser@sensemail.ch>
2024-09-24 22:26:15 +0200

    fix: fix popup min width on small screens

--- src/components/DaPopup.vue
+++ src/components/DaPopup.vue
@@ -2,11 +2,13 @@
   <section
     class="fixed left-0 top-0 w-screen h-screen bg-[#00000052] flex justify-center z-10 p-4 overflow-y-scroll"
   >
-    <div class="h-fit bg-bg p-8 rounded-[2rem] max-w-3xl min-w-[600px] animate-fade-in-top-fast">
+    <div
+      class="h-fit bg-bg p-8 rounded-[2rem] max-w-3xl min-w-full md:min-w-[600px] animate-fade-in-top-fast"
+    >
       <div class="flex justify-between items-center mb-4">
         <h1 class="font-bold text-xl">{{ title }}</h1>
         <span
-          v-if="showXmark"
+          v-if="showXmark && !isSmallScreen"
           class="bg-[#eee] p-4 flex items-center justify-center rounded-full transition-all duration-300 cursor-pointer hover:bg-[#ddd]"
           @click="$emit('close')"
         >
@@ -21,9 +23,12 @@
   </section>
 </template>
 <script lang="ts" setup>
+import { useMediaQuery } from '@vueuse/core'
 import { defineAsyncComponent } from 'vue'
 
 const XmarkIcon = defineAsyncComponent(() => import('./XmarkIcon.vue'))
+
+const isSmallScreen = useMediaQuery('(max-width: 640px)')
 
 withDefaults(
   defineProps<{
